812a4ec911ef01dd1599194252db37e5010b0de8
Barbara Laurince <blaurince@hotmail.com>
2023-05-04 16:09:33 -0400

    added dog list

--- index.js
+++ index.js
@@ -6,6 +6,38 @@ const app = express()
 app.use(express.json())
 app.use(cors())
 
+const dogs = [
+    {
+    name: 'Paco',
+    breed: 'Chihuahua',
+    age: 34,
+},
+
+
+{
+    name: 'Buba',
+    breed: 'Labrador',
+    age: 56,
+},
+
+{
+    name: 'Lucia',
+    breed: 'Yorkie',
+    age: 23,
+},
+
+{
+    name: 'Bob',
+    breed: 'Pittbul',
+    age: 69,
+},
+
+
+
+]
+
+
+
 app.get("/" , (req,res) => res.send('Welcome to my API'))
 
 app.listen(4000, () => console.log('Api running ❤️'))
